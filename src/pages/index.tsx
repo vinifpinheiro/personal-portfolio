e36202ddd024e2fb40aeb62629cab090aacd3bc8
VINICIUS PINHEIRO <viniciusfreitasp@hotmail.com>
2023-05-16 20:40:51 -0300

    base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Vinicius | Portfolio</title>
+      </Head>
+      
+    </>
+  );
+};
+
+export default Home;
